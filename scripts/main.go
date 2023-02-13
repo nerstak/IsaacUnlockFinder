eddc49d9b0fb6a76c524960bc1e6e19b9905e554
Nerstak <roy.karst@gmail.com>
2023-02-13 04:33:25 +0100

    feat: generating adjacency list

--- scripts/main.go
+++ scripts/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "gonum.org/v1/gonum/graph/encoding/dot"
   "log"
@@ -24,4 +25,6 @@ func generateJsonGraph() {
   dot.Unmarshal(file, graph)
   res := graph.AdjacencyList()
   fmt.Println(len(res))
+  txt, err := json.Marshal(res)
+  fmt.Println(string(txt))
 }
