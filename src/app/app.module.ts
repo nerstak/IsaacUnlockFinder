560ae84dc0220162cc6c6a9ad9d46415f3cafe09
Nerstak <roy.karst@gmail.com>
2023-02-16 22:55:37 +0100

    feat: adding pwa package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import { AchievementComponent } from './components/achievement/achievement.component';
 import {MatTabsModule} from "@angular/material/tabs";
 import { ListAchievementsComponent } from './components/list-achievements/list-achievements.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,13 @@ import { ListAchievementsComponent } from './components/list-achievements/list-a
         MatButtonModule,
         MatDividerModule,
         MatToolbarModule,
-        MatTabsModule
+        MatTabsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
   providers: [],
   bootstrap: [AppComponent]
